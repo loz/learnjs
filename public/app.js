73051515213d724e1971abed0731e56e9c5199ee
Jonathan Lozinski <jonathan.lozinski@arm.com>
2019-11-07 16:14:26 +0000

    Add application shell

--- public/app.js
+++ public/app.js
@@ -13,11 +13,27 @@ learnjs.problems = [
   }
 ];
 
+learnjs.template = function(name) {
+  return $('.templates .' + name).clone();
+};
+
 learnjs.applyObject = function(obj, elem) {
   for (var key in obj) {
     elem.find('[data-name="' + key + '"]').text(obj[key]);
   }
 };
+
+learnjs.buildCorrectFlash = function(problemNum) {
+  var correctFlash = learnjs.template('correct-flash');
+  var link = correctFlash.find('a');
+  if (problemNum < learnjs.problems.length) {
+    link.attr('href', '#problem-' + (problemNum + 1));
+  } else {
+    link.attr('href', '');
+    link.text("You're Finished!");
+  }
+  return correctFlash;
+}
 
 learnjs.flashElement = function(elem, content) {
   elem.fadeOut('fast', function() {
@@ -34,9 +50,13 @@ learnjs.appOnReady = function() {
   learnjs.showView(window.location.hash);
 }
 
+learnjs.landingView = function(data) {
+  return learnjs.template('landing-view');
+}
+
 learnjs.problemView = function(data) {
   var problemNumber = parseInt(data, 10);
-  var view = $('.templates .problem-view').clone();
+  var view = learnjs.template('problem-view');
   var title = 'Problem #' + problemNumber;
   var problemData = learnjs.problems[problemNumber - 1];
   var resultFlash = view.find('.result');
@@ -49,7 +69,8 @@ learnjs.problemView = function(data) {
 
   function checkAnswerClick() {
     if(checkAnswer()) {
-      learnjs.flashElement(resultFlash, 'Correct!');
+      var correctFlash = learnjs.buildCorrectFlash(problemNumber);
+      learnjs.flashElement(resultFlash, correctFlash);
     } else {
       learnjs.flashElement(resultFlash, 'Incorrect!');
     }
@@ -64,7 +85,9 @@ learnjs.problemView = function(data) {
 
 learnjs.showView = function(hash) {
   var routes = {
-    '#problem': learnjs.problemView
+    '#problem': learnjs.problemView,
+    '#': learnjs.landingView,
+    '': learnjs.landingView
   };
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
